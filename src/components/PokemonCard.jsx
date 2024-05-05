12231768d097604f3269fd662f3ba41d7fd8c913
hamid <hbo711772@gmail.com>
2024-05-05 22:51:54 +0200

    check 4

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,5 +1,10 @@
 function PokemonCard() {
-    return  <figure></figure>
+    return (
+     <figure>
+        <img 
+        src= "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="Bulbasaur" />
+    </figure>
+    );
 }
 
 export default PokemonCard;
