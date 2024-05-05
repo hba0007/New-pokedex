dd3b59483f94403b8fd49410af992f22170ced77
hamid <hbo711772@gmail.com>
2024-05-06 00:58:46 +0200

    check 7.6

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,8 +1,5 @@
-
-function PokemonCard(props) {
-    const pokemon = { name: "mew" };
-
-    console.log(props);
+/* eslint-disable react/prop-types */
+function PokemonCard({ pokemon }) {
     return (
      <figure>
         {pokemon.imgSrc != null ? (
