05b9de9021a8c329aac4da69115a4dd82d338522
hamid <hbo711772@gmail.com>
2024-05-05 22:55:04 +0200

    check 5

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -3,6 +3,7 @@ function PokemonCard() {
      <figure>
         <img 
         src= "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="Bulbasaur" />
+        <figcaption>Bulbasaur</figcaption>
     </figure>
     );
 }
