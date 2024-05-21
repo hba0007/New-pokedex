8353b22c8ccd763bc1712bcb43baba65a27ad27b
hamid <hbo711772@gmail.com>
2024-05-21 11:51:26 +0200

    Q7 done

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,21 +1,49 @@
-function NavBar ({ pokemonIndex, setPokemonIndex, pokemonList }) {
-    const previousIndex = () => {
-        setPokemonIndex(pokemonIndex - 1);
-       };
-
-       const nextIndex = () => {
-        setPokemonIndex(pokemonIndex + 1);
-       };
-    
+import Proptypes from "prop-types";
+import PokemonCard from "./PokemonCard";
 
 
-return (
-    <div className="button-container">
-        <button onClick={previousIndex}>Précédent</button>
-        
-        <button onClick={nextIndex}>Suivant</button>
-    </div>
-)
+function NavBar ({  pokemonIndex, setPokemonIndex , pokemonList  }) {
+
+    const previousIndex = () =>{setPokemonIndex(pokemonIndex -1 )};
+    const nextIndex = () =>{setPokemonIndex(pokemonIndex +1 )}
+    console.log(pokemonIndex);
+    return (
+        <div>
+  {pokemonIndex >0 && <button onClick={previousIndex}>Précédent</button>}
+  {pokemonIndex< pokemonList.length-1 && <button onClick={nextIndex}>Suivant</button>}
+     </div>
+    )
 }
+
+
+
+
+
+//return (
+//<div className="button-container">
+
+    //<button >Précédent</button>
+
+    //<button>Suivant</button>
+
+       //{pokemonList.map((pokemon, index) => (
+        //<button key={pokemon.name} onClick={() =>setPokemonIndex(index)}>
+           // {pokemon.name}</button>
+       //))}
+       //</div>
+       //);
+    //}
+
+//NavBar.propTypes = {
+    //PokemonCard: PokemonCard.number.isRequired,
+//     setPokemonIndex: Proptypes.func.isRequired,
+//     pokemonList: Proptypes.arrayOf(
+        
+//         PokemonCard.shape({
+//          name: Proptypes.string.isRequired,
+//          imgSrc: Proptypes.string,
+//         })
+// ).isRequired,
+// };
 
 export default NavBar;
